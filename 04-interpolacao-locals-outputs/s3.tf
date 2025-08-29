8d552c18e9a9e54578d39b8c1637d53badd53175
wffernandes <wffernandes01@gmail.com>
2025-08-29 18:02:49 -0300

    Adicionar object random

--- 04-interpolacao-locals-outputs/s3.tf
+++ 04-interpolacao-locals-outputs/s3.tf
@@ -5,8 +5,19 @@ resource "aws_s3_bucket" "this" {
 }
 
 resource "aws_s3_object" "this" {
-  bucket = aws_s3_bucket.this.bucket
-  key    = "config/${local.ip_filepath}"
-  source = local.ip_filepath
-  etag   = filemd5(local.ip_filepath)
+  bucket       = aws_s3_bucket.this.bucket
+  key          = "config/${local.ip_filepath}"
+  source       = local.ip_filepath
+  etag         = filemd5(local.ip_filepath)
+  content_type = "application/json"
+  tags         = local.common_tags
+}
+
+resource "aws_s3_object" "random" {
+  bucket       = aws_s3_bucket.this.bucket
+  key          = "config/${random_pet.bucket.id}.json"
+  source       = local.ip_filepath
+  etag         = filemd5(local.ip_filepath)
+  content_type = "application/json"
+  tags         = local.common_tags
 }
