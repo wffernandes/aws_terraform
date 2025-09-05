fed987fec6adbdb1a4611253b557ea3bbbc8b036
wffernandes <wffernandes01@gmail.com>
2025-09-05 17:23:57 -0300

    foreach-for-splat

---
+++ 02-terraform-intermediario/04-foreach-for-splat/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "1.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
